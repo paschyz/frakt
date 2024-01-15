475e32341f79de82c98a3a4977332f941f1d06ec
paschyz <paschy2002@gmail.com>
2024-01-15 03:11:22 +0100

    added send_fragment_result

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -1,13 +1,20 @@
 use log::{debug, error, info, trace, warn, LevelFilter};
 use serde::{Deserialize, Serialize};
-use shared::models::fragment::{
-    fragment_request::{self, FragmentRequest},
-    fragment_result::{self, FragmentResult},
-    fragment_task::{self, FragmentTask},
+use shared::models::{
+    fragment::{
+        fragment_request::{self, FragmentRequest},
+        fragment_result::{self, FragmentResult},
+        fragment_task::{self, FragmentTask},
+    },
+    pixel::pixel_data::PixelData,
+    point::Point,
+    range::Range,
+    resolution::Resolution,
+    u8_data::U8Data,
 };
 use std::{
     io::{Read, Write},
-    net::{SocketAddr, TcpStream},
+    net::{Shutdown, SocketAddr, TcpStream},
     thread,
 };
 
@@ -34,9 +41,70 @@ fn main() -> std::io::Result<()> {
             &mut data_response_message_size,
             &mut stream,
         );
-    }
+        let _ = stream.shutdown(Shutdown::Both);
+        loop {
+            thread::sleep(std::time::Duration::from_secs(1));
+            let mut stream = match connect_to_server(server, port) {
+                Ok(stream) => stream,
+                Err(e) => {
+                    println!("Failed to connect to server : {}", e);
+                    continue;
+                }
+            };
 
-    Ok(())
+            let mut fragment_result = FragmentResult {
+                id: {
+                    U8Data {
+                        offset: 4,
+                        count: 4,
+                    }
+                },
+                resolution: { Resolution { nx: 800, ny: 600 } },
+                range: {
+                    Range {
+                        min: Point { x: 0.5, y: 0.2 },
+                        max: Point { x: 0.8, y: 0.9 },
+                    }
+                },
+                pixels: {
+                    PixelData {
+                        offset: 100,
+                        count: 5000,
+                    }
+                },
+            };
+
+            match fragment_task {
+                Ok(ref task) => {
+                    fragment_result.id = task.id.clone();
+                    fragment_result.resolution = task.resolution.clone();
+                    fragment_result.range = task.range.clone();
+                }
+                Err(ref e) => {
+                    println!("error fragment_task{}", e);
+                    continue;
+                }
+            }
+
+            let _ = send_fragment_result(fragment_result, &mut stream);
+            let mut total_response_message_size = [0u8; 4]; // u8*4 = 32
+            let mut json_response_message_size = [0u8; 4];
+            let mut data_response_message_size = [0u8; 4];
+
+            let fragment_task: Result<FragmentTask, std::io::Error> = match receive_task(
+                &mut total_response_message_size,
+                &mut json_response_message_size,
+                &mut data_response_message_size,
+                &mut stream,
+            ) {
+                Ok(fragment_task) => Ok(fragment_task),
+                Err(e) => {
+                    println!("Failed to receive_task : {}", e);
+                    continue;
+                }
+            };
+        }
+    }
 }
 
 pub fn connect_to_server(server: &str, port: u16) -> Result<TcpStream, std::io::Error> {
@@ -64,17 +132,41 @@ pub fn send_fragment_request(
     stream.write(request_str.as_bytes())?; // JSON message
     Ok(())
 }
+pub fn send_fragment_result(
+    fragment_result: FragmentResult,
+    stream: &mut TcpStream,
+) -> std::io::Result<()> {
+    let fragment_result_json = serde_json::json!({"FragmentResult":fragment_result});
+    let result_str = serde_json::to_string(&fragment_result_json)?;
+    let offset_size = fragment_result.pixels.offset.to_be_bytes();
+    let count_size = fragment_result.pixels.count.to_be_bytes();
+    let total_size = (result_str.len() + offset_size.len() + count_size.len()) as u32;
+    let json_size = result_str.len() as u32;
 
+    println!("total_size: {:?}", total_size);
+    println!("result_str: {:?}", result_str);
+    println!("result_str.len(): {:?}", result_str.len());
+    println!("&total_size.to_be_bytes(): {:?}", &total_size.to_be_bytes());
+    stream.write(&total_size.to_be_bytes())?; // Total message size
+    stream.write(&json_size.to_be_bytes())?; // JSON message size
+    stream.write(result_str.as_bytes())?; // JSON message
+    stream.write(&offset_size)?; // Data
+    stream.write(&count_size)?; // Data
+
+    println!("{:?}", fragment_result.pixels);
+    Ok(())
+}
 pub fn receive_task(
     total_response_message_size: &mut [u8; 4],
     json_response_message_size: &mut [u8; 4],
     data_response_message_size: &mut [u8; 4],
     stream: &mut TcpStream,
 ) -> Result<FragmentTask, std::io::Error> {
+    println!("receive_task");
     stream.read_exact(total_response_message_size)?; // on lit le u32 total_size
     stream.read_exact(json_response_message_size)?; // on relit le u32 qui correspond au json message size
     let json_resonse_size = u32::from_be_bytes(*json_response_message_size);
-
+    println!("{:?}", json_resonse_size);
     let mut json_byte = vec![0u8; json_resonse_size as usize];
     stream.read_exact(&mut json_byte)?;
     let string_fragment_task = String::from_utf8_lossy(&json_byte); // string
